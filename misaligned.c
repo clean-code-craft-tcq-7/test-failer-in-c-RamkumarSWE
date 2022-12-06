efe5ffb73127071a9073403662bfc5a55ef44faa
RamkumarSWE <115966371+RamkumarSWE@users.noreply.github.com>
2022-12-06 21:49:25 +0530

    misalign updated

--- misaligned.c
+++ misaligned.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include <assert.h>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+#define MAX_COLOR_PAIR 25
+#define MAX_COLOR_LEN 15
+
+typedef struct {
+    char majorColor[MAX_COLOR_LEN];
+    char minorColor[MAX_COLOR_LEN];
+}ColorPair;
+
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+ColorPair colorPair[MAX_COLOR_PAIR];
+
+void printColorMap() {
+    int size, i;
+    size = fillColorMapList();
+
+    for (i = 0; i < size; i++) {
+        printf("%d | %s | %s\n", i, colorPair[i].majorColor, colorPair[i].minorColor);
+    }
+}
+
+int fillColorMapList() {
+
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            strcpy(colorPairList[(i*5)+j].majorColor, majorColor[i]);
+            strcpy(colorPairList[(i*5)+j].minorColor, minorColor[j]);
         }
     }
     return i * j;
